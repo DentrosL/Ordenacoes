2350a3366fc87d985d7f981ea48c008df2040fdd
Laiz Detros <megami.ay@gmail.com>
2026-08-19 19:47:01 -0300

    Add menu function for sorting algorithm selection

--- main.py
+++ main.py
@@ -4,4 +4,21 @@ array = [64, 34, 25, 12, 22, 11, 90]
 
 print("dados a serem ordenados:", array)
 
-# def menu 
+def menu():
+    print("Escolha o algoritmo de ordenação:")
+    print("1. Bubble Sort")
+    print("2. Quick Sort")
+    print("3. Insertion Sort")
+    print("4. Selection Sort")
+    print("5. Merge Sort")
+    print("6. Sair")
+
+# def bubble_sort
+# def quick_sort
+# def insertion_sort
+# def selection_sort
+# def merge_sort
+# def merge
+
+while run:
+  menu()
